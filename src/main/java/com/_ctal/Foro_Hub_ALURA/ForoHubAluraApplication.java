14fe433aa692cfc8407c6a0168ae48ece1761856
Camilo Ávila <98609727+8ctal@users.noreply.github.com>
2024-07-16 19:47:52 -0500

    1st Part of thhe Foro hub challenge

---
+++ src/main/java/com/_ctal/Foro_Hub_ALURA/ForoHubAluraApplication.java
@@ -0,0 +1,13 @@
+package com._ctal.Foro_Hub_ALURA;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ForoHubAluraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ForoHubAluraApplication.class, args);
+	}
+
+}
